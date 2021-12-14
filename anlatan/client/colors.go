f92c67634f2a9137cdbda0b738074c0109647d95
Wes Brown <wesbrown18@gmail.com>
2021-12-13 19:07:57 -0500

    Client code update.

--- anlatan/client/colors.go
+++ anlatan/client/colors.go
@@ -5,25 +5,36 @@ import (
 	"context"
 	"fmt"
 	"github.com/NovelAI/infrastructure/anlatan/completion"
+	"github.com/gdamore/tcell/v2"
 	"github.com/mazznoer/colorgrad"
 	"google.golang.org/grpc"
 	"io"
 	"log"
 	"math"
+	"strconv"
+	"strings"
 )
 
-func buildRequest() *completion.Request {
+var grad colorgrad.Gradient
+
+type Fragment struct {
+	text    string
+	buffer  string
+	chosen  *completion.LogProb
+	choices []*completion.LogProb
+}
+
+func buildRequest(prompt string, echo bool, numTokens int) *completion.Request {
 	var temperature = 0.8
 	var topP = 0.7
-	var maxTokens uint32 = 400
+	var maxTokens = uint32(numTokens)
 	var completions uint32 = 1
-	var logprobs uint32 = 20
-	var echo = true
+	var logprobs uint32 = 30
 
 	rq := completion.Request{
 		Prompt: []*completion.Prompt{
 			{Prompt: &completion.Prompt_String_{
-				String_: "The witch laughed",
+				String_: prompt,
 			}}},
 		//{Prompt: &completion.Prompt_String_{
 		//	String_: " goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose goose"}}},
@@ -50,6 +61,13 @@ func colorizeToken(logprob *completion.LogProb, gradient colorgrad.Gradient) str
 	}
 }
 
+func colorizeIndexedLogprob(tokenIdx uint32,
+	logProb *completion.LogProb) string {
+	return fmt.Sprintf("[\"%d\"]%s[\"\"]",
+		tokenIdx,
+		colorizeToken(logProb, grad))
+}
+
 func getLogProbStr(logprob *completion.LogProb) string {
 	tokenStr := logprob.GetToken().Text
 	if logprob.Logprob == nil {
@@ -60,7 +78,49 @@ func getLogProbStr(logprob *completion.LogProb) string {
 	}
 }
 
-type IndexedTokens map[uint][]*completion.LogProb
+func getPercentViewStr(logProb *completion.LogProb) string {
+	if logProb.Logprob != nil {
+		return fmt.Sprintf("%-5.2f %-10s",
+			math.Exp(*logProb.Logprob)*100.0,
+			colorizeToken(logProb, grad))
+	} else {
+		return fmt.Sprintf("%-5s %-10s",
+			"", logProb.Token.Text)
+	}
+}
+
+func getTokenViewStr(tokenIdx int, logProb *completion.LogProb) string {
+	return fmt.Sprintf("[\"%d\"]%s[\"\"]\n",
+		tokenIdx,
+		getPercentViewStr(logProb))
+}
+
+type IndexedFragments []*Fragment
+
+func (fragments IndexedFragments) getTextUntil(until int) string {
+	var buf strings.Builder
+	for idx := range fragments {
+		fragment := fragments[idx]
+		buf.WriteString(fragment.text)
+		if idx == until {
+			break
+		}
+	}
+	return buf.String()
+}
+
+func (fragments IndexedFragments) getBufferUntil(until int) string {
+	var buf strings.Builder
+	for idx := range fragments {
+		fragment := fragments[idx]
+		buf.WriteString(fmt.Sprintf("[\"%d\"]%s[\"\"]", idx,
+			fragment.buffer))
+		if idx == until {
+			break
+		}
+	}
+	return buf.String()
+}
 
 func main() {
 	// GRPC stuff
@@ -83,14 +143,68 @@ func main() {
 	textView.SetRegions(true)
 	textView.SetWordWrap(true)
 	textView.SetChangedFunc(func() {
-		app.Draw()
+		//app.Draw()
 	})
 
-	grad := colorgrad.RdBu()
-	print(grad.At(0).Hex())
+	tokenView := cview.NewTextView()
+	tokenView.SetDynamicColors(true)
+	tokenView.SetRegions(true)
+	tokenView.SetWordWrap(true)
+	tokenView.SetChangedFunc(func() {
+		//app.Draw()
+	})
 
-	go func() {
-		rq := buildRequest()
+	flex := cview.NewFlex()
+	flex.AddItem(tokenView, 25, 1, false)
+	flex.AddItem(textView, 0, 1, true)
+
+	focusManager := cview.NewFocusManager(func(p cview.Primitive) {})
+	focusManager.Add(tokenView, textView)
+
+	grad = colorgrad.RdBu()
+
+	indexedFragments := make(IndexedFragments, 0)
+	viewedTokenIdx := 0
+	//numSelections := 0
+
+	updateTokenView := func(tokenIdx int) {
+		tokenView.Clear()
+		tokenView.Highlight()
+
+		if indexedFragments[tokenIdx].chosen != nil {
+			fmt.Fprintf(tokenView,
+				"%s\n---------------------\n",
+				getPercentViewStr(indexedFragments[tokenIdx].chosen))
+		} else {
+			fmt.Fprintf(tokenView,
+				"%-5s %-10s\n---------------------\n",
+				"", indexedFragments[tokenIdx].text)
+		}
+
+		choicesList := indexedFragments[tokenIdx].choices
+		if choicesList != nil {
+			for choicesIdx := range choicesList {
+				logProb := choicesList[choicesIdx]
+				if logProb.Logprob != nil {
+					fmt.Fprint(tokenView,
+						fmt.Sprintf("[\"%d\"]%-5.2f %-10s[\"\"]\n",
+							choicesIdx,
+							math.Exp(*logProb.Logprob)*float64(100.0),
+							colorizeToken(logProb, grad)))
+				}
+			}
+		}
+		viewedTokenIdx = tokenIdx
+	}
+
+	bufferState := ""
+	storyText := ""
+	tokenCt := 0
+	receiving := true
+	// subPos := 0
+
+	backendRequest := func(rq *completion.Request) {
+		receiving = true
 		stream, err := client.Completion(context.Background(), rq)
 		if err != nil {
 			log.Fatal(err)
@@ -104,49 +218,169 @@ func main() {
 			if err != nil {
 				log.Fatalf("%v.Completion(_) = _, %v", client, err)
 			}
-
 			logprobs := answer.Choices[0].GetLogprobs()
 			tokens := logprobs.GetTokens()
-			// topN := logprobs.GetTop()
+			topN := logprobs.GetTop()
 			for tokenIdx := range tokens.Logprobs {
-				chosenStr := colorizeToken(tokens.Logprobs[tokenIdx], grad)
-				// tokenChoices := topN[tokenIdx]
-				fmt.Fprint(textView, chosenStr)
-				// if tokenChoices != nil {
-				//	topNentries := tokenChoices.GetLogprobs()
-				//	for topNentryIdx := range topNentries {
-				//		fmt.Fprint(textView, getLogProbStr(topNentries[topNentryIdx]), " ")
-				//	}
-				//}
+				appended := false
+				fragment := &Fragment{
+					text: tokens.Logprobs[tokenIdx].Token.Text,
+					buffer: colorizeToken(tokens.Logprobs[tokenIdx],
+						grad),
+					chosen:  tokens.Logprobs[tokenIdx],
+					choices: topN[tokenIdx].Logprobs,
+				}
+				if len(indexedFragments) > tokenCt {
+					indexedFragments[tokenCt] = fragment
+				} else {
+					indexedFragments = append(indexedFragments, fragment)
+					appended = true
+				}
+
+				if appended {
+					tokenRepr := fmt.Sprintf("[\"%d\"]%s[\"\"]", tokenCt,
+						fragment.buffer)
+					bufferState += tokenRepr
+					fmt.Fprint(textView, tokenRepr)
+				} else {
+					textView.SetText(indexedFragments.getBufferUntil(
+						len(indexedFragments)))
+				}
+				textView.Highlight()
+				textView.Highlight(strconv.Itoa(tokenCt))
+				updateTokenView(tokenCt)
+				tokenCt += 1
+				app.Draw()
 			}
 		}
-	}()
-	/*
-		textView.SetDoneFunc(func(key tcell.Key) {
-			currentSelection := textView.GetHighlights()
-			if key == tcell.KeyEnter {
-				if len(currentSelection) > 0 {
-					textView.Highlight()
-				} else {
-					textView.Highlight("0")
-					textView.ScrollToHighlight()
-				}
-			} else if len(currentSelection) > 0 {
-				index, _ := strconv.Atoi(currentSelection[0])
-				if key == tcell.KeyTab {
-					index = (index + 1) % numSelections
-				} else if key == tcell.KeyBacktab {
-					index = (index - 1 + numSelections) % numSelections
-				} else {
-					return
-				}
-				textView.Highlight(strconv.Itoa(index))
-				textView.ScrollToHighlight()
-			}
-		}) */
+		indexedFragments = indexedFragments[:tokenCt]
+		textView.SetText(indexedFragments.getBufferUntil(len(indexedFragments)))
+		app.Draw()
+		receiving = false
+	}
+	go backendRequest(buildRequest("The witch laughed", true,
+		200))
 
+	var priorSelection string
+
+	tokenSelectAction := func(action cview.MouseAction,
+		event *tcell.EventMouse) (cview.MouseAction, *tcell.EventMouse) {
+		if receiving {
+			return action, event
+		}
+		if action == cview.MouseLeftClick && tokenView.HasFocus() {
+			// x, y := event.Position()
+			currentSelection := tokenView.GetHighlights()
+			if currentSelection == nil {
+				return action, event
+			}
+			index, _ := strconv.Atoi(currentSelection[0])
+			tokenList := indexedFragments[viewedTokenIdx].choices
+			selectedToken := tokenList[index]
+			indexedFragments[viewedTokenIdx].chosen = selectedToken
+			indexedFragments[viewedTokenIdx].text = selectedToken.Token.Text
+			indexedFragments[viewedTokenIdx].buffer =
+				colorizeIndexedLogprob(uint32(viewedTokenIdx), selectedToken)
+
+			bufferState = indexedFragments.getBufferUntil(len(indexedFragments))
+			storyText = indexedFragments.getTextUntil(viewedTokenIdx)
+			textView.SetText(bufferState)
+			//textView.Clear()
+			//fmt.Fprint(textView, bufferState)
+			app.Draw()
+			tokenCt = viewedTokenIdx + 1
+			go backendRequest(buildRequest(storyText, false,
+				200))
+		}
+		return action, event
+	}
+
+	tokenMouseAction := func(action cview.MouseAction,
+		event *tcell.EventMouse) (cview.MouseAction, *tcell.EventMouse) {
+		if !receiving {
+			app.QueueUpdateDraw(func() {
+				currentSelection := textView.GetHighlights()
+				if priorSelection != currentSelection[0] {
+					currTokenIdx, _ := strconv.Atoi(currentSelection[0])
+					updateTokenView(currTokenIdx)
+					priorSelection = currentSelection[0]
+				}
+			})
+		}
+		return action, event
+	}
+
+	// bufferDirty := false
+
+	textEditorAction := func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyRune {
+			inputRune := event.Rune()
+			currFragment := indexedFragments[viewedTokenIdx]
+			if currFragment.chosen != nil {
+				currFragment.chosen = nil
+				currFragment.text = string(inputRune)
+				currFragment.buffer = currFragment.text
+			} else {
+				currFragment.text += string(inputRune)
+				currFragment.buffer = currFragment.text
+			}
+			textView.SetText(indexedFragments.getBufferUntil(
+				len(indexedFragments)))
+			updateTokenView(viewedTokenIdx)
+		}
+		if event.Key() == tcell.KeyDelete {
+			indexedFragments = append(indexedFragments[:viewedTokenIdx],
+				indexedFragments[viewedTokenIdx+1:]...)
+			textView.SetText(indexedFragments.getBufferUntil(
+				len(indexedFragments)))
+			if viewedTokenIdx > len(indexedFragments)-1 {
+				viewedTokenIdx--
+			}
+			textView.Highlight(strconv.Itoa(viewedTokenIdx))
+			updateTokenView(viewedTokenIdx)
+		}
+		if event.Key() == tcell.KeyEnter {
+			tokenCt = 0
+			go backendRequest(buildRequest(
+				indexedFragments.getTextUntil(viewedTokenIdx),
+				true, 200))
+		}
+		return event
+	}
+
+	tokenView.SetMouseCapture(tokenSelectAction)
+	textView.SetMouseCapture(tokenMouseAction)
+	textView.SetInputCapture(textEditorAction)
+
+	textView.SetDoneFunc(func(key tcell.Key) {
+		currentSelection := textView.GetHighlights()
+		if key == tcell.KeyEnter {
+			focusManager.Focus(tokenView)
+		} else if len(currentSelection) > 0 {
+			index, _ := strconv.Atoi(currentSelection[0])
+			if key == tcell.KeyTab {
+				if index >= len(indexedFragments)-1 {
+					index = 0
+				} else {
+					index = index + 1
+				}
+			} else if key == tcell.KeyBacktab {
+				index = index - 1
+			} else {
+				return
+			}
+			textView.Highlight(strconv.Itoa(index))
+			textView.ScrollToHighlight()
+			updateTokenView(index)
+		}
+		app.Draw()
+	})
+
+	tokenView.SetBorder(true)
+	tokenView.SetVisible(true)
 	textView.SetBorder(true)
-	app.SetRoot(textView, true)
+	app.SetRoot(flex, true)
+
 	if err := app.Run(); err != nil {
 		panic(err)
 	}
---
+++ anlatan/client/go.mod
@@ -0,0 +1,36 @@
+module github.com/NovelAI/infrastructure/anlatan/client
+
+go 1.17
+
+require (
+	code.rocketnine.space/tslocum/cview v1.5.7
+	github.com/NovelAI/infrastructure/anlatan/completion v0.0.0-00010101000000-000000000000
+	github.com/wbrown/novelai-research-tool v0.0.0-20211122022657-a2c85df08669
+	google.golang.org/grpc v1.42.0
+)
+
+require (
+	code.rocketnine.space/tslocum/cbind v0.1.5 // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210828201608-73703f7ed490 // indirect
+	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/jdkato/prose/v2 v2.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.14-0.20210830053702-dc8fe66265af // indirect
+	github.com/mazznoer/colorgrad v0.8.1 // indirect
+	github.com/mazznoer/csscolorparser v0.1.0 // indirect
+	github.com/mingrammer/commonregex v1.0.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sys v0.0.0-20210831042530-f4d43177bf5e // indirect
+	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gonum.org/v1/gonum v0.7.0 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/neurosnap/sentences.v1 v1.0.6 // indirect
+)
+
+replace github.com/NovelAI/infrastructure/anlatan/completion => ../completion
