d2d4df41ce3a4d397fea1a0ab604dba4b1d9c3aa
Andrew Senetar <asenetar@coreweave.com>
2022-08-05 20:01:51 +0000

    fix: Get the ts/js to generate
    - Add local protoc for the js/ts as apparently the current version is
      broken.
    - Add package.json which was missing causing failures
    - Add updated ts/js files
    - Add more CMake .gitignore values
    - Generate client stubs (service ones)
    - Generate missing grpc js files
    - Updates to CMakeLists
      - Uncomment ts related items from makefile
      - Reorganize js/ts section

--- gooseai/completion/completion_grpc_pb.d.ts
+++ gooseai/completion/completion_grpc_pb.d.ts
@@ -1,39 +1,23 @@
-// package: anlatan
+// GENERATED CODE -- DO NOT EDIT!
+
+// package: gooseai
 // file: completion.proto
 
-/* tslint:disable */
-/* eslint-disable */
-
-import * as grpc from "grpc";
 import * as completion_pb from "./completion_pb";
+import * as grpc from "grpc";
 
 interface ICompletionServiceService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
-    completion: ICompletionServiceService_ICompletion;
-}
-
-interface ICompletionServiceService_ICompletion extends grpc.MethodDefinition<completion_pb.Request, completion_pb.Answer> {
-    path: "/anlatan.CompletionService/Completion";
-    requestStream: false;
-    responseStream: true;
-    requestSerialize: grpc.serialize<completion_pb.Request>;
-    requestDeserialize: grpc.deserialize<completion_pb.Request>;
-    responseSerialize: grpc.serialize<completion_pb.Answer>;
-    responseDeserialize: grpc.deserialize<completion_pb.Answer>;
+  completion: grpc.MethodDefinition<completion_pb.Request, completion_pb.Answer>;
 }
 
 export const CompletionServiceService: ICompletionServiceService;
 
-export interface ICompletionServiceServer {
-    completion: grpc.handleServerStreamingCall<completion_pb.Request, completion_pb.Answer>;
+export interface ICompletionServiceServer extends grpc.UntypedServiceImplementation {
+  completion: grpc.handleServerStreamingCall<completion_pb.Request, completion_pb.Answer>;
 }
 
-export interface ICompletionServiceClient {
-    completion(request: completion_pb.Request, options?: Partial<grpc.CallOptions>): grpc.ClientReadableStream<completion_pb.Answer>;
-    completion(request: completion_pb.Request, metadata?: grpc.Metadata, options?: Partial<grpc.CallOptions>): grpc.ClientReadableStream<completion_pb.Answer>;
-}
-
-export class CompletionServiceClient extends grpc.Client implements ICompletionServiceClient {
-    constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
-    public completion(request: completion_pb.Request, options?: Partial<grpc.CallOptions>): grpc.ClientReadableStream<completion_pb.Answer>;
-    public completion(request: completion_pb.Request, metadata?: grpc.Metadata, options?: Partial<grpc.CallOptions>): grpc.ClientReadableStream<completion_pb.Answer>;
+export class CompletionServiceClient extends grpc.Client {
+  constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
+  completion(argument: completion_pb.Request, metadataOrOptions?: grpc.Metadata | grpc.CallOptions | null): grpc.ClientReadableStream<completion_pb.Answer>;
+  completion(argument: completion_pb.Request, metadata?: grpc.Metadata | null, options?: grpc.CallOptions | null): grpc.ClientReadableStream<completion_pb.Answer>;
 }
