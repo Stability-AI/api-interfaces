2c44d3cdc8ace9e40f4026e710078b9d31c0c70e
Wes Brown <wesbrown18@gmail.com>
2022-08-14 15:03:18 -0400

    Regenerate stubs.

--- gooseai/generation/generation_pb.d.ts
+++ gooseai/generation/generation_pb.d.ts
@@ -686,9 +686,11 @@ export const Upscaler: UpscalerMap;
 
 export interface ActionMap {
   ACTION_PASSTHROUGH: 0;
-  ACTION_REGENERATE: 1;
-  ACTION_OBFUSCATE: 2;
-  ACTION_DISCARD: 3;
+  ACTION_REGENERATE_DUPLICATE: 1;
+  ACTION_REGENERATE: 2;
+  ACTION_OBFUSCATE_DUPLICATE: 3;
+  ACTION_OBFUSCATE: 4;
+  ACTION_DISCARD: 5;
 }
 
 export const Action: ActionMap;
