3ff543e94ae4e03cf56a6ac75eacddcc2b6fde4c
Wes Brown <wesbrown18@gmail.com>
2022-09-01 13:22:57 -0400

    Regenerate protobufs.

--- gooseai/generation/generation_pb.d.ts
+++ gooseai/generation/generation_pb.d.ts
@@ -366,6 +366,34 @@ export namespace ConditionerParameters {
   }
 }
 
+export class ScheduleParameters extends jspb.Message {
+  hasStart(): boolean;
+  clearStart(): void;
+  getStart(): number;
+  setStart(value: number): void;
+
+  hasEnd(): boolean;
+  clearEnd(): void;
+  getEnd(): number;
+  setEnd(value: number): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ScheduleParameters.AsObject;
+  static toObject(includeInstance: boolean, msg: ScheduleParameters): ScheduleParameters.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ScheduleParameters, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ScheduleParameters;
+  static deserializeBinaryFromReader(message: ScheduleParameters, reader: jspb.BinaryReader): ScheduleParameters;
+}
+
+export namespace ScheduleParameters {
+  export type AsObject = {
+    start: number,
+    end: number,
+  }
+}
+
 export class StepParameter extends jspb.Message {
   getScaledStep(): number;
   setScaledStep(value: number): void;
@@ -374,6 +402,11 @@ export class StepParameter extends jspb.Message {
   clearSampler(): void;
   getSampler(): SamplerParameters | undefined;
   setSampler(value?: SamplerParameters): void;
+
+  hasSchedule(): boolean;
+  clearSchedule(): void;
+  getSchedule(): ScheduleParameters | undefined;
+  setSchedule(value?: ScheduleParameters): void;
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): StepParameter.AsObject;
@@ -389,6 +422,7 @@ export namespace StepParameter {
   export type AsObject = {
     scaledStep: number,
     sampler?: SamplerParameters.AsObject,
+    schedule?: ScheduleParameters.AsObject,
   }
 }
 
